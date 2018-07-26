dc3e085df2bba3580fa13b1ca86d41e43cac12f4
Charlotte Driscoll <charlotte.d.driscoll@icloud.com>
2018-07-26 16:19:24 -0500

    nav scroll change

--- index.js
+++ index.js
@@ -40,3 +40,21 @@ $(document).ready(function(){
     $it = setInterval(value, $timer);
       
   });
+
+
+  ///// NAVSCROLL /////
+  $(function() {
+    var header = $(".navbar");
+  
+    $(window).scroll(function() {    
+        var scroll = $(window).scrollTop();
+        if (scroll >= 50) {
+            header.addClass("scrolled");
+        } else {
+            header.removeClass("scrolled");
+        }
+    });
+  
+});
+
+////////
